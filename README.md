d8b83f75646057a2fbb25416b94747ddd4ee3474
Mirza Merdovic <mirzamerdovic@gmail.com>
2018-05-27 15:14:34 +0200

    Fixed #5 - Added release-notes.md - Updated README.md - Updated vsixmanifest information. - Added response type information for Swagger.

--- README.md
+++ README.md
@@ -1,11 +1,4 @@
 Web API Visual Studio Template.
 
 VS template for Web API with OWIN projects. 
-
-It is created from empty Web API project template and it includes:
- - CORS configuration
- - Bad route handling
- - Error handling through IExceptionHandler
- - Error logging via IExceptionLogger (you still need to implement your favorite logger)
- - Autofac container configuration
- - Simple home "screen", well not a screen but a message that states that Web API hase started successfully.
+Please refer to [Wiki](https://github.com/MirzaMerdovic/WebApiStartTemplate/wiki) page for more information.
--- WebApiStarter/WebApiStarter.Template/Controllers/TestController.cs
+++ WebApiStarter/WebApiStarter.Template/Controllers/TestController.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Web.Http;
+using System.Web.Http.Description;
 
 namespace WebApiStarter.Template.Controllers
 {
@@ -10,7 +11,7 @@ namespace WebApiStarter.Template.Controllers
     [RoutePrefix("v1/test")]
     public class TestController : ApiController
     {
-        [HttpPost, Route("")]
+        [HttpPost, Route(""), ResponseType(typeof(int))]
         public IHttpActionResult Post(string value)
         {
             var id = new Random().Next();
@@ -18,15 +19,18 @@ namespace WebApiStarter.Template.Controllers
             return CreatedAtRoute("GetById", new { id }, id);
         }
 
-        [HttpGet, Route("")]
+        [HttpGet, Route(""), ResponseType(typeof(List<string>))]
         public IHttpActionResult Get()
         {
             return Ok(new List<string> {"Test 1", "Test 2"});
         }
 
-        [HttpGet, Route("{id:int}", Name = "GetById")]
+        [HttpGet, Route("{id:int}", Name = "GetById"), ResponseType(typeof(int))]
         public IHttpActionResult Get(int id)
         {
+            if (id <= 0)
+                return NotFound();
+
             return Ok(id * id);
         }
     }
